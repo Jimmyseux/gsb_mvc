b3b248ba013206e15a6d972be28e4f40918dc2aa
kelignoel2 <kelignoel32@gmail.com>
2016-11-24 12:07:23 +0100

    modifcation de v_tablette à finir

--- LSW42GSBmvc/vues/v_tablette.php
+++ LSW42GSBmvc/vues/v_tablette.php
@@ -1,10 +1,12 @@
 <?php
-    <table>
-      <tr>
-        <td> Date Affectation </td>
-        <td> ID Tablette </td>
-      </tr>
+    $reponce = GSB-MVC( "SELECT * FROM ");
+    <table >
+        <tr>
+          <th>Date affectation</th>
+          <th>Numéro tablette <th>
+        </tr>
     </table>
+    // https://openclassrooms.com/forum/sujet/afficher-le-contenu-d-une-base-de-donnee-dans-un-tableau-69984
 
 
  ?>
