62aacc488c3fd086a7784a182e171c55b6a26571
kelignoel2 <kelignoel32@gmail.com>
2016-11-30 16:39:19 +0100

    suite du talbeau

--- LSW42GSBmvc/vues/v_tablette.php
+++ LSW42GSBmvc/vues/v_tablette.php
@@ -1,18 +1,19 @@
-<h3>Les tablette de <?php echo $prenom $nom  </h3> 
+<h3>Les tablette de <?php echo $prenom $nom?>  </h3>
     $reponce = GSB-MVC( "SELECT * FROM tablette");
-    <table >
+    <table class="table tablette" >
+      <caption> Voilà le tableau </caption>
         <tr>
           <td>Date affectation</td>
           <td>Numéro tablette <td>
         </tr>
 
-
+        <?php while ()
         <tr>
+
           <td></td>
           <td></td>
         </tr>
     </table>
-    // https://openclassrooms.com/forum/sujet/afficher-le-contenu-d-une-base-de-donnee-dans-un-tableau-69984
-
+    //https://openclassrooms.com/forum/sujet/afficher-donnees-bdd-dans-tableau-html
 
  ?>
